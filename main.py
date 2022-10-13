6173d8e253a5a052bbdde904853273f9b49203a4
Adersh Babu <65764036+AdershBabu@users.noreply.github.com>
2022-10-13 19:18:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import time # Importing timer library
+import pyautogui# Importing gui library
+time.sleep(4)# Initiate timer
+count=0 # Initializing count=0
+while count<=1000:  # starting loop for contiuous action
+    pyautogui.typewrite("My name is Adersh , I am a Pythonistas") #what to print
+    pyautogui.press("Enter") #Setting initiating setup
+    count=count+1 #Incrementing count by one
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
